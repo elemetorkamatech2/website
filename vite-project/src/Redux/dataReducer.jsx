ca4eb2f930944dea84a61cbf6f2d5959e0ec0e3d
chayaRivkaErenfeld <erenfeld226@gmail.com>
2023-05-31 00:28:49 +0300

    fix

---
+++ vite-project/src/Redux/dataReducer.jsx
@@ -0,0 +1,22 @@
+import { produce } from 'immer';
+
+const InitialState = {
+  users: [
+    { id: 1, name: 'avraham', mail: 'avraham@gmail.com', password: 1234 },
+  ],
+};
+export const dataReducer = produce((state = InitialState, action) => {
+  switch (action.type) {
+    case 'SET_USERS':
+      state.users = action.payload;
+      break;
+    case 'ADD_NEW_USER':
+      state.users.push(action.payload);
+      break;
+    case 'SET_DETAILS_USER':
+      state.users = action.payload;
+      break;
+    default:
+      return state;
+  }
+}, InitialState);
