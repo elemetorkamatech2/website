110afec7bef0bebccc75e63a3dd5028d9137b240
tzivyaGit <tzivya791@gmail.com>
2023-06-01 12:57:43 +0300

    change

--- vite-project/src/Api/api.jsx
+++ vite-project/src/Api/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getUsersApi = async () => {
   try {
-    const response = await axios.get(import.meta.env.VITE_URL_API);
+    const response = await axios.get('http://localhost:3003/UserRoute/getAll');
     return response.data;
   } catch (err) {
     console.log(err);
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import dotenv from 'dotenv'
+dotenv.config()
 export default defineConfig({
   plugins: [react()],
+  //port: Number(process.env.VITE_URL_API) || 3003
+
 })
