8c634462172c36663600449c7b09f81932b5b76b
RivkiLevin <133571804+RivkiLevin@users.noreply.github.com>
2023-06-06 17:01:21 +0300

    try 5:00

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import { Provider } from 'react-redux'
-import { ourStore } from './Redux/Store'
+import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ourStore } from './Redux/Store';
 import { AllUser } from './Components/allUsers';
 
 function App() {
-  return (
-    <>
-      <Provider store={ourStore}>
-         <AllUser />
-        <h1>bsd 😂</h1>
-      </Provider>
-    </>
-  )
+	return (
+		<>
+			<Provider store={ourStore}>
+				<AllUser />
+				<h1>bsd 😂</h1>
+			</Provider>
+		</>
+	);
 }
 
-export default App;;;;;;;
+export default App;
--- vite-project/src/Components/allUsers.jsx
+++ vite-project/src/Components/allUsers.jsx
@@ -1,30 +1,33 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable indent */
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import React from 'react';
 import getUsersApi from '../Api/api';
 import { SetUsers } from '../Redux/action/';
 
 export function AllUser() {
-  const dispcath = useDispatch();
-  useEffect(() => {
-    async function fetchData() {
-      const usersFromServer = await getUsersApi();
-      dispcath(SetUsers(usersFromServer));
-    }
-    fetchData();
-  }, []);
-  const usersFromStore = useSelector((myStoer) => myStoer.users);
-  return (
-    <>
-      {
-        usersFromStore !== undefined && usersFromStore.length > 0
+	const dispcath = useDispatch();
+	useEffect(() => {
+		async function fetchData() {
+			const usersFromServer = await getUsersApi();
+			dispcath(SetUsers(usersFromServer));
+		}
+		fetchData();
+	}, []);
+	const usersFromStore = useSelector((myStoer) => myStoer.users);
+	return (
+		<>
+			{
+				usersFromStore !== undefined && usersFromStore.length > 0
         && (
-        <h1>
-          {' '}
+        	<h1>
+        	{' '}
           num users:
           { usersFromStore.length }
-        </h1>
+</h1>
         )
-    }
-    </>
-  );
+			}
+		</>
+	);
 }
