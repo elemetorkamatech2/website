3d00f81a35fd88acfec47d76e7d7b75321e17e13
RivkiLevin <133571804+RivkiLevin@users.noreply.github.com>
2023-06-07 10:51:39 +0300

    rivky_is_the_best

--- src/Api/api.jsx
+++ src/Api/api.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const getUsersApi = async () => {
-  try {
-    const response = await axios.get(import.meta.env.VITE_URL_API);
-    return response.data;
-  } catch (err) {
-    console.log(err);
-    return null;
-  }
+	try {
+		const response = await axios.get(import.meta.env.VITE_URL_API);
+		return response.data;
+	} catch (err) {
+		console.log(err);
+		return null;
+	}
 };
 export default getUsersApi;
