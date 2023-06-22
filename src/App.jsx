bfe7a8eb7414d21bdebd49fba8701bc7272f76cc
RivkiLevin <133571804+RivkiLevin@users.noreply.github.com>
2023-06-22 12:25:30 +0300

    page_main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react';
 import { AllWebsite } from './Components/allWebsite';
+import { Page } from './Components/Page';
+
 function App() {
 	return (
 		<>
+	<Page></Page>
 			<AllWebsite></AllWebsite>
+		
 		</>
 	);
 }
