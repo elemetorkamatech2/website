34c4ff28180f373a06be4df7a8af09af8f4a6692
RivkiLevin <133571804+RivkiLevin@users.noreply.github.com>
2023-07-04 02:21:18 +0300

    k

--- src/Components/editDetails.jsx
+++ src/Components/editDetails.jsx
@@ -1,69 +1,144 @@
-import { Button } from "./Button"
+import { Button } from './Button';
 import { useDispatch, useSelector } from 'react-redux';
-import { SetWeb } from "../Redux/action";
-import { useState } from "react";
-import './editDetails.css'
+import { SetWeb } from '../Redux/action';
+import { useState } from 'react';
+import './editDetails.css';
+import React from 'react';
 
-export const EditDetails = (props)=>{
+export const EditDetails = ()=>{
 
-    const dispatch = useDispatch();
-    const [myweb,setMyweb]=useState(
-        useSelector(state => state.web[0]));
-    // const web = useSelector((myStoer) => myStoer.web);
-    console.log(myweb)
-     const dispatchMyweb = () => {
-
-        dispatch(SetWeb(myweb));
-      };
-      const dispatchName = (e) => {
-        setMyweb({ ...myweb, name: e });
-      };
-        const dispatchAdress = (e) => {
-            setMyweb({ ...myweb, adress: e });
-        };
-            const dispatchCategury = (e) => {
-                setMyweb({ ...myweb, categury: e });
-            };
+	const dispatch = useDispatch();
+	const [myweb,setMyweb]=useState(
+		useSelector(state => state.web[0]));
+	console.log(myweb);
+	const dispatchMyweb = () => {
+		dispatch(SetWeb(myweb));
+	};
+	
+	const dispatchtitle = (e) => {
+		setMyweb({ ...myweb, title: e });
+	};
+	const dispatchdescription = (e) => {
+		setMyweb({ ...myweb, description: e });
+	};
+	const dispatchdomain = (e) => {
+		setMyweb({ ...myweb, domain: e });
+	};
+	const dispatchtypeOfDomain = (e) => {
+		setMyweb({ ...myweb, typeOfDomain: e });
+	};
+	const dispatchcpu = (e) => {
+		setMyweb({ ...myweb, cpu: e });
+	};
+	const dispatchmemory = (e) => {
+		setMyweb({ ...myweb, memory: e });
+	};
+	const dispatchstatus = (e) => {
+		setMyweb({ ...myweb, status: e });
+	};
+	const dispatchbackups = (e) => {
+		setMyweb({ ...myweb, backups: e });
+	};
+	const dispatchuserId = (e) => {
+		setMyweb({ ...myweb, userId: e });
+	};
        
-    return<>
-      <div class="container">
-    <form action="/action_page.php">
-    <div class="row">
-      <div class="col-25">
-        <label for="fname"> Name</label>
-      </div>
-      <div class="col-75">
-        <input type="text" id="fname" name="firstname" placeholder={myweb.name} onChange={(e) => dispatchName(e.target.value)}/>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-25">
-        <label for="ladress">Adress</label>
-      </div>
-      <div class="col-75">
-        <input type="text" id="ladress" name="lastname"  placeholder={myweb.adress}onChange={(e) =>dispatchAdress(e.target.value)}/>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-25">
-        <label for="categury">categury</label>
-      </div>
-      <div class="col-75">
-        <select id="categury" name="country">
-          <option value={myweb.categury}>{myweb.categury} onChange={(e) =>dispatchCategury(e.target.value)}</option>
-          <option value="games">games</option>
-          <option value="Other">Other</option>
-        </select>
-      </div>
-    </div>
-    <br/>
-    <div class="row">
-    <Button size="small" onClick={() =>{alert("נשמר"),dispatchMyweb()}} label="submit" />
-    </div>
-    </form>
-  </div>
+	return<>
+		<div className="container">
+			<label htmlFor="fname"> פרטי האתר שלי</label>
+			<form action="/action_page.php">
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="fname"> title</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="fname" name="firstname" placeholder={myweb.title} onChange={(e) => dispatchtitle(e.target.value)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="ladress">description</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="ladress" name="lastname"  placeholder={myweb.description}onChange={(e) =>dispatchdescription(e.target.value)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="ladress">domain</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="ladress" name="lastname"  placeholder={myweb.domain}onChange={(e) =>dispatchdomain(e.target.value)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="ladress">typeOfDomain</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="ladress" name="lastname"  placeholder={myweb.typeOfDomain}onChange={(e) =>dispatchtypeOfDomain(e.target.value)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="ladress">cpu</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="ladress" name="lastname"  placeholder={myweb.cpu}onChange={(e) =>dispatchcpu(e.target.value)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="ladress">memory</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="ladress" name="lastname"  placeholder={myweb.memory}onChange={(e) =>dispatchmemory(e.target.value)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="ladress">status</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="ladress" name="lastname"  placeholder={myweb.status}onChange={(e) =>dispatchstatus(e.target.value)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="ladress">backups</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="ladress" name="lastname"  placeholder={myweb.backups}onChange={(e) =>dispatchbackups(e.target.value)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-25">
+						<label htmlFor="ladress">userId</label>
+					</div>
+					<div className="col-75">
+						<input type="text" id="ladress" name="lastname"  placeholder={myweb.userId}onChange={(e) =>dispatchuserId(e.target.value)}/>
+					</div>
+				</div>
+				{/* <div className="row">
+					<div className="col-25">
+						<label htmlFor="categury">categury</label>
+					</div>
+					<div className="col-75">
+						<select id="categury" name="country">
+							<option value={myweb.categury}>{myweb.categury} onChange={(e) =>dispatchCategury(e.target.value)}</option>
+							<option value="games">games</option>
+							<option value="Other">Other</option>
+						</select>
+					</div>
+				</div> */}
+				<br/>
+				<div className="row">
+					<Button size="small" onClick={() =>{alert('נשמר'),dispatchMyweb();}} label="submit" />
+				</div>
+			</form>
+		</div>
 
         
-    </>
-    }
+	</>;
+};
    
--- src/Components/viewDetails.jsx
+++ src/Components/viewDetails.jsx
@@ -1,10 +1,25 @@
-import { Button } from "./Button"
-import {  useNavigate } from "react-router"
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { Button } from './Button';
+import './Details.css';
+import React from 'react';
 
-export const Details = ()=>{
-    let navigate = useNavigate()
+export const Details = () => {
+	const data = useSelector(state => state.web);
+	let navigate = useNavigate();
+	console.log(data);
+	return (
+		<>
+			<div id='Details'>
+				{data.map(item => (
+					<div key={item.id}>
+						<h2>{item.title}</h2>
+						<p>{item.description}</p>
+					</div>
+				))}
+			</div>
+			<Button  primary size="small" onClick={() =>navigate('/EditDetails')} label="edit" />
+		</>
+	);
+};
 
-    return<>
-        <Button  primary size="small" onClick={() =>navigate("/EditDetails")} label="edit" />
-    </>
-    }
