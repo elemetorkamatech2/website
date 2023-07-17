5595b145f427a4085f718d20eef9d38dbef78c35
chayaRivkaErenfeld <erenfeld226@gmail.com>
2023-07-17 17:06:13 +0300

    add

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from './Button';
+// import { Button } from './Button';
 import './header.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Login } from './login';
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
     <header>
