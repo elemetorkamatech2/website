6c274a4f78a7eca50795e79468e9f7ec847f7536
tzivyaGit <tzivya791@gmail.com>
2023-07-19 10:39:12 +0300

    getByUserIdAndFix

--- src/Components/Website/allWebsite.jsx
+++ src/Components/Website/allWebsite.jsx
@@ -2,22 +2,29 @@ import {React, useState, useEffect } from 'react';
 import { GetWebsitesApi } from '../../Api/api';
 import { useSelector } from 'react-redux';
 import '../EditDetails/editDetails.css';
+import { useNavigate } from 'react-router-dom';
 
 export const AllWebsite = () => {
+
+	let navigate = useNavigate();
 	const userId = useSelector((myStoer) => myStoer.userId);
 	const [websites, setWebsites] = useState([]);
+	
 	useEffect(() => {
 		const fetchData = async () => {
 			const data = await GetWebsitesApi(userId);
-			console.log(data);
 			setWebsites(data);
 		};
 		fetchData();
 	}, []);
+
+	const viewDetails = (w) => {
+		navigate('/Details', { state: { website: w } });
+	  };
 	return <>
 		{
 			(websites !== null && websites.length > 0) ? (
-				websites.map((w) => <button key={w._id}>{w.title}</button>)
+				websites.map((w) => <button onClick={() => viewDetails(w)} key={w._id}>{w.title}</button>)
 			) : (
 				<>
 					<h1>אין נתונים</h1>
